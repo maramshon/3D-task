65baf846f6c0cc594071489e9400b48e963787ee
maramshon <mralshon@yahoo.com>
2018-02-20 08:43:29 +0300

    refactor the code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	// Step 2: The Scene and the Renderer
+	// Step 1: The Scene and the Renderer
 
 	// store the window's width and height in variables
 	var width = window.innerWidth;
@@ -9,16 +9,13 @@ $(function(){
 	//define the renderer and the scene:
 	var renderer = new THREE.WebGLRenderer({ antialias: true});
 	renderer.setSize(width, height);
-	// document.body.appendChild(renderer.domElement);
-	$('body').append(renderer.domElement)  // -> jQuery
+	$('body').append(renderer.domElement)  
 
 	var scene = new THREE.Scene;
 
-	//Step 3: The Cube
+	//Step 2: The Cube
 
 	var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
-	// var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
-
 	var cubeMaterials = [ 
 	    new THREE.MeshBasicMaterial({color:0xff0000, transparent:true, opacity:0.8, side: THREE.DoubleSide}),
 	    new THREE.MeshBasicMaterial({color:0x00ff00, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
@@ -27,7 +24,6 @@ $(function(){
 	    new THREE.MeshBasicMaterial({color:0xff00ff, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
 	    new THREE.MeshBasicMaterial({color:0x00ffff, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
 	]; 
-	// Create a MeshFaceMaterial, which allows the cube to have different materials on each face 
 	var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
 
 	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
@@ -36,23 +32,19 @@ $(function(){
 	 
 	scene.add(cube);
 
-	// Step 4: Camera!
+	// Step 3: Camera!
 
 	var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
 
-	// camera.position.x = 0;
 	camera.position.y = 160;
 	camera.position.z = 400;
-
-	// add the camera to the scene and render it:
 
 	scene.add(camera);
 
 	camera.lookAt(cube.position);
 
-	// Step 5: Lights!
+	// Step 4: Lights!
 
-	//*******************************************
 	var skyboxGeometry = new THREE.CubeGeometry(1000, 1000, 1000);
 	var cubeMaterials = [ 
 	    new THREE.MeshBasicMaterial({color:0xff9999, transparent:true, opacity:0.8, side: THREE.DoubleSide}), //right
@@ -62,18 +54,11 @@ $(function(){
 	    new THREE.MeshBasicMaterial({color:0xff00ff, transparent:true, opacity:0.0, side: THREE.DoubleSide}), //front
 	    new THREE.MeshBasicMaterial({color:0x99ffcc, transparent:true, opacity:0.8, side: THREE.DoubleSide}), //back
 	]; 
-	// Create a MeshFaceMaterial, which allows the cube to have different materials on each face 
 	var skyboxMaterial = new THREE.MeshFaceMaterial(cubeMaterials, {side: THREE.BackSide });
 	var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-	// skybox.rotation.y = Math.PI * 45 / 180;
+	
 	skybox.position.y = 400;
-	// skybox.position.z = -100;
-	//************************************
-
-	// var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
-	// var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide });
-	// var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-	 
+		 
 	scene.add(skybox);
 
 	var pointLight = new THREE.PointLight(0xffffff);
@@ -81,25 +66,35 @@ $(function(){
 	 
 	scene.add(pointLight);
 
-	// var clock = new THREE.Clock;
+	// Step 5: Action!
+
 	$('body').keydown(function(event) {
+
+		// Press S => move forward the player
 		if(event.which === 83){
 			cube.position.z += 4;
 			cube.rotation.x -= Math.PI*90/180;	
 		}
+
+		// Press W => move forward the screen
 		if(event.which === 87){
 			cube.position.z -= 4;
 			cube.rotation.x += Math.PI*90/180;	
 		}
+
+		// Press A => move to the left
 		if(event.which === 65){
 			cube.position.x -= 6;
 			cube.rotation.y += Math.PI*90/180;	
 		}
+
+		// Press D => move to the right
 		if(event.which === 68){
 			cube.position.x += 6;
 			cube.rotation.y -= Math.PI*90/180;	
 		}
 	});
+	
 	function render() {
 		renderer.render(scene, camera);
 	    
